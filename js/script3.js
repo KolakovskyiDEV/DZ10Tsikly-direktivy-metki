28fc3715c1b18de428eee559c3dbf1f41aa945db
Artem Kolakovskyi <kolakovskyiartem@gmail.com>
2021-09-02 16:50:12 +0300

    Create script3.js

---
+++ js/script3.js
@@ -0,0 +1,17 @@
+//3.
+// for (let i = 1; i <= 100; i++){
+//     if(i % 2 == 0){
+//     console.log(i);
+//         }
+// }
+
+for (let i = 2; i <= 100; i += 2){
+    
+
+  
+     if((i-1) % 2 == 0){
+    console.log(i-1);
+        } else if ((i % 2) == 0) {
+           console.log(i); 
+        }
+}
